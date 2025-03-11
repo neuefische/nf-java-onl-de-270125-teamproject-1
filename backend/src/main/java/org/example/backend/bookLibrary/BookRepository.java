ccf743348bf01fe51cf753f0bb1393cd769281d4
mwilmes <maximilian.wilmes@hdi.de>
2025-03-11 16:33:10 +0100

    add get endpoint

---
+++ backend/src/main/java/org/example/backend/bookLibrary/BookRepository.java
@@ -0,0 +1,9 @@
+package org.example.backend.bookLibrary;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends MongoRepository<Book, String> {
+
+}
