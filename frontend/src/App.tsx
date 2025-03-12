c024605e8edd84870ed88ca2b36fadd9b6c16eb5
mwilmes <maximilian.wilmes@hdi.de>
2025-03-12 12:46:44 +0100

    FRONTEND: add post endpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import BooksPage from "./pages/BooksPage.tsx";
 export default function App() {
     return (
         <Routes>
-            <Route path="/" element={<p>wefwefwef</p>}/>
+            <Route path="/" element={<h1>Home</h1>}/>
             <Route path="/books" element={<BooksPage/>}/>
         </Routes>
     );
