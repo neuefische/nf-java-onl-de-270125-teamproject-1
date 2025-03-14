abc7adcfd3a72dc4c4275d73427da0009bf111f3
kevinldg <kevinloeding@outlook.de>
2025-03-14 16:11:20 +0100

    FRONTEND: Add book details page - Add link in book component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import {Route, Routes} from "react-router-dom";
 import BooksPage from "./pages/BooksPage.tsx";
+import BookIdPage from "./pages/BookIdPage.tsx";
 
 export default function App() {
     return (
@@ -7,6 +8,7 @@ export default function App() {
             <Route path="/" element={<h1>Home</h1>}/>
             <Route path="/books" element={<BooksPage/>}/>
             <Route path="/books/add" element={<BooksPage/>}/> {/*add component*/}
+            <Route path="/books/:id" element={<BookIdPage/>}/>
         </Routes>
     );
 }
