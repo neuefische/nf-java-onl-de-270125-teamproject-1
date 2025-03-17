bda965eb98467ae445dd847342cfb668c6cbb8c1
mwilmes <maximilian.wilmes@hdi.de>
2025-03-17 15:26:07 +0100

    FRONTEND: add put endpoint to frontend

--- frontend/src/pages/BookIdPage.tsx
+++ frontend/src/pages/BookIdPage.tsx
@@ -1,4 +1,4 @@
-import {Link, useParams} from "react-router-dom";
+import {Link, useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 import {useEffect, useState} from "react";
 import {Book} from "../types/Book.ts";
@@ -6,13 +6,23 @@ import {Book} from "../types/Book.ts";
 export default function BookIdPage() {
     const params = useParams();
     const bookId = params.id;
+    const navigate = useNavigate();
 
     const [book, setBook] = useState<Book>({id: "", title: "", author: ""});
+
 
     const fetchBook = () => {
         axios.get(`/api/books/${bookId}`)
             .then(response => setBook(response.data))
             .catch(error => console.error("Error fetching book: ", error));
+    };
+
+    const updateBook = () => {
+        axios.put(`/api/books/${bookId}`, book)
+            .then(() => {
+                console.log(book)
+            })
+        navigate("/books")
     };
 
     useEffect(() => {
@@ -21,9 +31,19 @@ export default function BookIdPage() {
 
     return (
         <div>
-            <p>Id: {book.id}</p>
-            <p>Title: {book.title}</p>
-            <p>Author: {book.author}</p>
+            <form onSubmit={updateBook}>
+                <label>Id</label>
+                <input type={"text"} value={book.id} disabled={true}/>
+
+                <label>Title</label>
+                <input type={"text"} value={book.title}
+                       onChange={(event) => setBook({...book, title: event.target.value})}/>
+
+                <label>Author</label>
+                <input type={"text"} value={book.author}
+                       onChange={(event) => setBook({...book, author: event.target.value})}/>
+                <button>Update Book</button>
+            </form>
             <Link to="/books">Back to books</Link>
         </div>
     );
