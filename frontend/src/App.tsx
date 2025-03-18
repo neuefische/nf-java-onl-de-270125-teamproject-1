a143b171d3ca32d44ee237ce193e2ab35651c606
mwilmes <maximilian.wilmes@hdi.de>
2025-03-18 16:25:27 +0100

    FRONTEND: add styling, navigation and "Home" page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import {Route, Routes} from "react-router-dom";
 import BooksPage from "./pages/BooksPage.tsx";
 import BookIdPage from "./pages/BookIdPage.tsx";
+import Header from "./components/Header.tsx";
+import Home from "./pages/Home.tsx";
 
 export default function App() {
     return (
+        <>
+        <Header/>
         <Routes>
-            <Route path="/" element={<h1>Home</h1>}/>
+            <Route path="/" element={<Home/>}/>
             <Route path="/books" element={<BooksPage/>}/>
-            <Route path="/books/add" element={<BooksPage/>}/> {/*add component*/}
             <Route path="/books/:id" element={<BookIdPage/>}/>
         </Routes>
+        </>
     );
 }
--- frontend/src/components/Book.tsx
+++ frontend/src/components/Book.tsx
@@ -1,19 +1,22 @@
-import { Book as BookType } from "../types/Book.ts";
+import {Book as BookType} from "../types/Book.ts";
 import {Link} from "react-router-dom";
 
 type BookProps = {
     book: BookType;
-    deleteBook: (id:string) => void
+    deleteBook: (id: string) => void
 }
 
-export default function Book({ book, deleteBook}: BookProps) {
+export default function Book({book, deleteBook}: BookProps) {
     return (
-        <div>
-            <p>Title: {book.title}</p>
-            <p>Author: {book.author}</p>
-
-            <button onClick={()=>{deleteBook(book.id)}}>x</button>
-            <Link to={`/books/${book.id}`}>Details</Link>
+        <div className={"book-item"}>
+            <div className={"book-info"}>
+                <p>Title: {book.title}</p>
+                <p>Author: {book.author}</p>
+            </div>
+            <div className={"book-utility"}>
+                <button className={"delete-book-button"} onClick={() => {deleteBook(book.id)}}>delete</button>
+                <Link to={`/books/${book.id}`}>Details</Link>
+            </div>
         </div>
     );
 }
