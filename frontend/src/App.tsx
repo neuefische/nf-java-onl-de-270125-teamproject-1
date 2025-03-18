8580d64b30ea2cbc90907eaa37dca20d6838965c
mwilmes <maximilian.wilmes@hdi.de>
2025-03-18 12:20:23 +0100

    OAUTH2: implement oauth2 login to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import {Route, Routes} from "react-router-dom";
 import BooksPage from "./pages/BooksPage.tsx";
 import BookIdPage from "./pages/BookIdPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<h1>Home</h1>}/>
-            <Route path="/books" element={<BooksPage/>}/>
-            <Route path="/books/add" element={<BooksPage/>}/> {/*add component*/}
-            <Route path="/books/:id" element={<BookIdPage/>}/>
-        </Routes>
+        <>
+            <LoginPage/>
+            <Routes>
+                <Route path="/" element={<h1>Home</h1>}/>
+                <Route path="/books" element={<BooksPage/>}/>
+                <Route path="/books/add" element={<BooksPage/>}/> {/*add component*/}
+                <Route path="/books/:id" element={<BookIdPage/>}/>
+            </Routes>
+        </>
+
     );
 }
